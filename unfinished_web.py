ca69b87f5866ac98b24487fb2c5e3bd42ed5aa7f
NikkiG183 <nicole@gloudemans.net>
2021-01-20 12:37:18 -0500

    web app updates

--- unfinished_web.py
+++ unfinished_web.py
@@ -11,7 +11,7 @@ Created on Mon Sep  7 23:06:03 2020
 
 from threading import Lock
 from flask import Flask, render_template, session, request, \
-    copy_current_request_context, Response
+    copy_current_request_context, Response, url_for, redirect
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
 import random
@@ -35,6 +35,8 @@ socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 
+#will want to pass the id of the page into the get methods for retrieving data
+
 
 def background_thread():
     """Example of how to send server generated events to clients."""
@@ -52,15 +54,30 @@ def background_thread():
                       namespace='/test')
 
 
-@app.route('/')
-def index():
-    # location = ['Red', 'Blue', 'Black', 'Orange']
-    return render_template('fancy_index.html', async_mode=socketio.async_mode) #, colours=colours)
-
 @app.route('/', methods=['GET'])
-def dropdown():
-    colours = ['Red', 'Blue', 'Black', 'Orange']
-    return render_template('.html', colours=colours)
+def index():
+    locations = ['All', 'Rand', 'MRB3', 'Commons']
+    return render_template('fancy_index.html', async_mode=socketio.async_mode , title = "All", locations = locations)
+
+
+
+@app.route('/<id1>', methods=['GET', 'POST'])
+def cam(id1):
+    titles = id1
+    locations = ['All', 'Rand', 'MRB3', 'Commons']
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('index'))
+
+    # show the form, it wasn't submitted
+    return render_template('fancy_index.html', async_mode=socketio.async_mode , title = titles, locations = locations)
+
+@app.route('/downloads', methods=['GET', 'POST'])
+def downloads():
+    return render_template('downloads.html', async_mode=socketio.async_mode)
 
 
 
@@ -81,7 +98,7 @@ if __name__ == '__main__':
         # global ocpts
         # global dists
 
-        '10.66.46.173/16'
+        # '10.66.46.173/16'
         socketio.run(app, host='127.0.0.1', port=8000, debug=True,
                               use_reloader=False)
 
